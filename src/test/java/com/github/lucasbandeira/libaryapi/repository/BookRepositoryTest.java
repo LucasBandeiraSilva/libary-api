6187050b4a946211d085f708195cdd80eb406139
Lucas Bandeira <lucasbandeirai40@gmail.com>
2024-11-25 15:53:22 -0300

    Test: operations using cascade

--- src/test/java/com/github/lucasbandeira/libaryapi/repository/BookRepositoryTest.java
+++ src/test/java/com/github/lucasbandeira/libaryapi/repository/BookRepositoryTest.java
@@ -29,11 +29,56 @@ public class BookRepositoryTest {
         book.setTitle("UFO");
         book.setPublicationDate(LocalDate.of(2020,2,1));
 
-        Author author = authorRepository.findById(UUID.fromString("d79958d3-db05-45c2-9e66-c736cede74e9")).orElse(null);
+        Author author = authorRepository
+                .findById(UUID.fromString("d79958d3-db05-45c2-9e66-c736cede74e9"))
+                .orElse(null);
         book.setAuthor(author);
 
         bookRepository.save(book);
 
+    }
+
+    @Test
+    public void saveAuthorAndBookTest(){
+        Book book = new Book();
+        book.setIsbn("36745-62561");
+        book.setPrice(BigDecimal.valueOf(100));
+        book.setGender(BookGender.FICTION);
+        book.setTitle("Third Book");
+        book.setPublicationDate(LocalDate.of(2020,2,1));
+
+        Author author = new Author();
+
+        author.setName("Bruce");
+        author.setNationality("Brazilian");
+        author.setBirthdate(LocalDate.of(1977, 03, 01));
+
+        authorRepository.save(author);
+
+        book.setAuthor(author);
+
+
+        bookRepository.save(book);
+
+    }
+
+    @Test
+    public void saveCascadeTest(){
+        Book book = new Book();
+        book.setIsbn("36745-62561");
+        book.setPrice(BigDecimal.valueOf(100));
+        book.setGender(BookGender.FICTION);
+        book.setTitle("Other Book");
+        book.setPublicationDate(LocalDate.of(2020,2,1));
+
+        Author author = new Author();
+
+        author.setName("Steve");
+        author.setNationality("Brazilian");
+        author.setBirthdate(LocalDate.of(1977, 03, 01));
+        book.setAuthor(author);
+
+        bookRepository.save(book);
 
     }
 }
