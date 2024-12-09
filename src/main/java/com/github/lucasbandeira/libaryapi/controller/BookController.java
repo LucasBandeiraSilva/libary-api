4bc9d13321b23c5a060d2be2d982593899331afc
Lucas Bandeira <lucasbandeirai40@gmail.com>
2024-12-09 14:07:52 -0300

    Feat: Book Register endpoint with Bean Validation

--- src/main/java/com/github/lucasbandeira/libaryapi/controller/BookController.java
+++ src/main/java/com/github/lucasbandeira/libaryapi/controller/BookController.java
@@ -1,16 +1,34 @@
 package com.github.lucasbandeira.libaryapi.controller;
 
+import com.github.lucasbandeira.libaryapi.dto.BookRegisterDTO;
+import com.github.lucasbandeira.libaryapi.dto.ErrorResponse;
+import com.github.lucasbandeira.libaryapi.exceprions.DuplicateRegisterException;
 import com.github.lucasbandeira.libaryapi.service.BookService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("books")
+@RequestMapping("/books")
 public class BookController {
 
     private final BookService bookService;
+
+
+    @PostMapping
+    public ResponseEntity <Object> save(@RequestBody @Valid BookRegisterDTO bookRegisterDTO ){
+        try {
+            return ResponseEntity.ok(bookRegisterDTO);
+        } catch (DuplicateRegisterException e) {
+            var dtoError = ErrorResponse.conflict(e.getMessage());
+            return  ResponseEntity.status(dtoError.status()).body(dtoError);
+        }
+    }
 
 
 }
--- src/main/java/com/github/lucasbandeira/libaryapi/dto/BookRegisterDTO.java
+++ src/main/java/com/github/lucasbandeira/libaryapi/dto/BookRegisterDTO.java
@@ -1,17 +1,27 @@
 package com.github.lucasbandeira.libaryapi.dto;
 
 import com.github.lucasbandeira.libaryapi.model.BookGender;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import org.hibernate.validator.constraints.ISBN;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
 public record BookRegisterDTO(
+        @ISBN(message = "Please provide a valid ISBN!")
+        @NotBlank(message = "Required field!")
         String isbn,
+        @NotBlank(message = "Required field!")
         String title,
+        @NotNull(message = "Required field!")
+        @Past(message = "It Cannot be a future date!")
         LocalDate publicationDate,
         BookGender gender,
         BigDecimal price,
+        @NotNull(message = "Required field!")
         UUID authorId
 ) {
 }
