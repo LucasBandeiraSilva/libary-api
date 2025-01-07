4ad93e2a8221e7b9d05b7a21c046774ea1467820
Lucas Bandeira <lucasbandeirai40@gmail.com>
2025-01-07 09:12:14 -0300

    Feat: Creating username structure in the database

---
+++ src/main/java/com/github/lucasbandeira/libaryapi/model/Username.java
@@ -0,0 +1,28 @@
+package com.github.lucasbandeira.libaryapi.model;
+
+import io.hypersistence.utils.hibernate.type.array.ListArrayType;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.Type;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table
+@Data
+public class Username {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String login;
+
+    private String password;
+
+    @Type(ListArrayType.class)
+    @Column(columnDefinition = "varchar[]")
+    private List <String> roles;
+
+}
