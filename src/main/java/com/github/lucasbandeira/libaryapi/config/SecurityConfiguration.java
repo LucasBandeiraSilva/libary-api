3b0fb25aa89feb1e1afb461e5df71f04df2a8f51
Lucas Bandeira <lucasbandeirai40@gmail.com>
2025-01-24 10:35:12 -0300

    feat: enable and test Swagger Open API in the project

--- src/main/java/com/github/lucasbandeira/libaryapi/config/SecurityConfiguration.java
+++ src/main/java/com/github/lucasbandeira/libaryapi/config/SecurityConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
@@ -54,6 +55,17 @@ public class SecurityConfiguration {
                 .oauth2ResourceServer(oAuth2ResourceServer -> oAuth2ResourceServer.jwt(Customizer.withDefaults()))
                 .addFilterAfter(jwtCustomAuthenticationFilter, BearerTokenAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return web ->
+          web.ignoring().requestMatchers("/v2/api-docs/**",
+                  "/v3/api-docs/**",
+                  "/swagger-resources/**",
+                  "/swagger-ui.html/**",
+                  "/swagger-ui/**",
+                  "/webjars/**");
     }
 
 
